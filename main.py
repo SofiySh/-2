bd199e9ff3b880964d7803be882724f5ed2bcbdb
SofiySh <proskurnina.sofy@yandex.ru>
2024-05-31 17:32:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+print('Задача «Длина слова»')
+a = 'Good health to all'
+print(len('Good health to all'))
+print('Задача «Суммы и разности»')
+first = 576
+second = 156
+print(first + second)
+print(first - second)
+summa = 732
+diff = 420
+print('Задача «Среднее арифметическое»')
+first = 124.0
+second = 577.0
+third = 532.0
+print((first + second + third)/3.0)
+mean = 411.0
+print('Задача «Простые строчки»')
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + str(', ') + first_string)
+print('Задача «Сложная формула»')
+a = 5.0
+b = 2.0
+c = 7.0
+print((a * b)+(a * c))
+f = 45.0
+print((f ** 3.0) / 2.0)
+
